d766f00b34b58c57d7d4180067ff03819a9baa3c
captain2010 <87228533+captain2010@users.noreply.github.com>
2022-02-13 19:50:34 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -39,19 +39,19 @@ function check()
      check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>" ;
      row = question_number + input_box + check_button ;
      
-    if (answer==word)
+    if (get_answer==actual_answer)
     {
         if ( Answer_turn=="player1")
         {
-            player1_score=player1_score+1;
-            document.getElementById("player1_score").innerHTML= player1_score;
+            Update_player1_score=player1_score+1;
+            document.getElementById("player1_score").innerHTML= update_player1_score;
 
         }
         else
         {
 
-            player2_score=player2_score+1;
-            document.getElementById("player2_score").innerHTML= player2_score;
+            update_player2_score=player2_score+1;
+            document.getElementById("player2_score").innerHTML= update_player2_score;
 
         }
     }
